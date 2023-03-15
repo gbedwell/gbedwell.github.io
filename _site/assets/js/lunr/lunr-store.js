f7b3123c421723583c093c12e185891425b4c791
Gregory Bedwell <gregoryjbedwell@gmail.com>
2023-03-15 10:10:05 -0400

    build site

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -6,7 +6,7 @@ var store = [{
         "teaser": null
       },{
         "title": "Evaluating the sums of arbitrary negative binomial distributions",
-        "excerpt":"Introduction to the NB distribution The negative binomial (NB) distribution is widely used to model count data whose variance is greater than expected given a standard binomial or Poisson distributions. While the NB distribution has been used across scientific disciplines, the usage that is most relevant to my interests is...","categories": [],
+        "excerpt":"Introduction to the NB distribution The negative binomial (NB) distribution is widely used to model count data whose variance is greater than expected given a standard binomial or Poisson distribution. The NB distribution has been used to model count data across scientific disciplines. The usage that is most relevant to...","categories": [],
         "tags": [],
         "url": "/nb-convolutions/",
         "teaser": null
